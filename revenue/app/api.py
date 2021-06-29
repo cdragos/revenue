e2e6fd615f3f4c179d2bf5529fb1ec9d89c67a78
Dragos Catarahia <dragos@catarahia.me>
2021-06-29 20:44:57 +0300

    Add daily endpoint.

--- revenue/app/api.py
+++ revenue/app/api.py
@@ -1,4 +1,4 @@
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
 from flask import (
     Blueprint,
     jsonify,
@@ -14,6 +14,9 @@ from .models import Entry, Company, db
 api = Blueprint('api', __name__, url_prefix='/api')
 
 
+_DATE_FORMAT = '%Y-%m-%d'
+
+
 @api.route('/hourly')
 def hourly():
     start_date = request.args.get('start_date')
@@ -21,7 +24,7 @@ def hourly():
         raise ValidationException('Start date is required.')
 
     try:
-        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        start_date = datetime.strptime(start_date, _DATE_FORMAT)
     except (ValueError, TypeError):
         raise ValidationException('Start date is not a valid date.')
 
@@ -50,5 +53,44 @@ def hourly():
 
 @api.route('/daily')
 def daily():
-    # TODO
-    return jsonify(msg="implement me")
+    start_date = request.args.get('start_date')
+    if not start_date:
+        raise ValidationException('Start date is required.')
+
+    end_date = request.args.get('end_date')
+    if not start_date:
+        raise ValidationException('End date is required.')
+
+    try:
+        start_date = datetime.strptime(start_date, _DATE_FORMAT)
+    except (ValueError, TypeError):
+        raise ValidationException('Start date is not a valid date.')
+
+    try:
+        end_date = datetime.strptime(end_date, _DATE_FORMAT)
+    except (ValueError, TypeError):
+        raise ValidationException('End date is not a valid date.')
+
+    company_id = request.args.get('company_id')
+    company = get_object_or_404(Company, Company.id == company_id)
+
+    delta = end_date - start_date
+    days = {
+        (start_date + timedelta(days=i)).strftime(_DATE_FORMAT):
+        '{:0.2f}'.format(0) for i in range(delta.days + 1)
+    }
+
+    entries = Entry.select(
+        fn.strftime('%Y-%m-%d', Entry.created).alias('day'),
+        fn.SUM(Entry.gross_total).alias('total')
+    ).group_by(
+        fn.date_trunc('day', Entry.created)
+    ).where(
+        Entry.company == company,
+        Entry.created.between(start_date, end_date + timedelta(days=1))
+    ).dicts()[:]
+
+    for entry in entries:
+        days[entry['day']] = '{:0.2f}'.format(entry['total'])
+
+    return jsonify({'results': days})
--- revenue/tests/test_app.py
+++ revenue/tests/test_app.py
@@ -105,3 +105,97 @@ def test_get_hourly_missing_date(client):
 def test_get_hourly_invalid_company(client):
     resp = client.get('/api/hourly?start_date=2018-01-10&company_id=1')
     assert resp.status_code == 404
+
+
+@freeze_time("2018-01-10 01:00:01")
+def test_get_daily(client):
+    company = Company.create(name='Nory')
+    customer = Customer.create(first_name='John', last_name='Doe')
+    user_id = 1
+
+    now = datetime.now()
+    entries = [{
+        'company': company,
+        'customer': customer,
+        'net_total': Decimal(95.55),
+        'gross_total': Decimal(108.45),
+        'created': now,
+        'user_id': user_id,
+    }, {
+        'company': company,
+        'customer': customer,
+        'net_total': Decimal(60.08),
+        'gross_total': Decimal(68.9),
+        'created': now,
+        'user_id': user_id,
+    }, {
+        'company': company,
+        'customer': customer,
+        'net_total': Decimal(49.82),
+        'gross_total': Decimal(59.05),
+        'created': now + timedelta(hours=1),
+        'user_id': user_id,
+    }, {
+        'company': company,
+        'customer': customer,
+        'net_total': Decimal(30.51),
+        'gross_total': Decimal(35.5),
+        'created': now + timedelta(hours=1),
+        'user_id': user_id,
+    }, {
+        'company': company,
+        'customer': customer,
+        'net_total': Decimal(97.12),
+        'gross_total': Decimal(113.5),
+        'created': now + timedelta(days=1),
+        'user_id': user_id,
+    }, {
+        'company': company,
+        'customer': customer,
+        'net_total': Decimal(119.5),
+        'gross_total': Decimal(136.55),
+        'created': now + timedelta(days=2),
+        'user_id': user_id,
+    }, {
+        'company': company,
+        'customer': customer,
+        'net_total': Decimal(6.22),
+        'gross_total': Decimal(7.4),
+        'created': now + timedelta(days=2, hours=1),
+        'user_id': user_id,
+    }]
+    Entry.insert_many(entries).execute()
+
+    resp = client.get(
+        f'/api/daily?start_date=2018-01-10&end_date=2018-01-12&company_id={company.id}')
+    assert resp.status_code == 200
+    assert resp.json == {
+        'results': {
+            '2018-01-10': '271.90',
+            '2018-01-11': '113.50',
+            '2018-01-12': '143.95',
+        }
+    }
+
+
+def test_get_daily_invalid_start_date(client):
+    resp = client.get('/api/daily?start_date=invalid')
+    assert resp.status_code == 400
+    assert resp.json == {
+        'detail': 'Validation Error',
+        'errors': 'Start date is not a valid date.'
+    }
+
+
+def test_get_daily_missing_date(client):
+    resp = client.get('/api/daily')
+    assert resp.status_code == 400
+    assert resp.json == {
+        'detail': 'Validation Error',
+        'errors': 'Start date is required.'
+    }
+
+
+def test_get_daily_invalid_company(client):
+    resp = client.get('/api/daily?start_date=2018-01-10&end_date=2018-01-12&company_id=1')
+    assert resp.status_code == 404
